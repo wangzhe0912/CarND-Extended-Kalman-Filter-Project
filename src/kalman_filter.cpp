a9e46d948efc82e5223fcea89f7ff129a29d962e
wangzhe0912 <wangzhe0912@tju.edu.cn>
2018-10-02 18:34:00 +0800

    fix bug and get the final result

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -31,9 +31,15 @@ void KalmanFilter::Predict() {
   TODO:
     * predict the state
   */
+  cout << "I will predict" << endl;
+  cout << "input: F_: " << F_ << endl;
+  cout << "input: x_: " << x_ << endl;
+  cout << "input: Q_: " << Q_ << endl;
   x_ = F_ * x_;
   MatrixXd Ft = F_.transpose();
   P_ = F_ * P_ * Ft + Q_;
+  cout << "output: x_: " << x_ << endl;
+  cout << "output: P_: " << P_ << endl;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -42,17 +48,24 @@ void KalmanFilter::Update(const VectorXd &z) {
     * update the state by using Kalman Filter equations
     * Laser
   */
+  cout << "Update Begin: " << endl;
+  cout << "x_: " << x_ << endl;
   VectorXd y = z - H_Laser_ * x_;
+  cout << "H: " << H_Laser_ << endl;
+  cout << "y: " << y << endl;
+  cout << "R: " << R_Laser_ << endl;
   MatrixXd Ht = H_Laser_.transpose();
   MatrixXd S = H_Laser_ * P_ * Ht + R_Laser_;
-  MatrixXd Si = S.inverse();
-  MatrixXd K =  P_ * Ht * Si;
+  MatrixXd K =  P_ * Ht * S.inverse();
+  MatrixXd I = MatrixXd::Identity(x_.size(), x_.size());
+
 
   //new state
   x_ = x_ + (K * y);
-  long x_size = x_.size();
-  MatrixXd I = MatrixXd::Identity(x_size, x_size);
   P_ = (I - K * H_Laser_) * P_;
+  cout << "Update output: " << endl;
+  cout << "output: x_: " << x_ << endl;
+  cout << "output: P_: " << P_ << endl;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
@@ -63,10 +76,6 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   */
   cout << "UpdateEKF Begin: " << endl;
   Tools tools;
-
-  cout << "x_: " << x_ << endl;
-  MatrixXd Hj = tools.CalculateJacobian(x_);
-  cout << "Hj: " << Hj << endl;
   double rho = sqrt(x_(0) * x_(0) + x_(1) * x_(1));
   double phi = atan2(x_(1), x_(0));
   double rho_dot = (fabs(rho) < 0.0001) ? 0 : (x_(0) * x_(2) + x_(1) * x_(3)) / rho;
@@ -74,26 +83,25 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   VectorXd x_polar(3);
   x_polar << rho, phi, rho_dot;
   cout << "x_polar: " << x_polar << endl;
+
+  MatrixXd Hj = tools.CalculateJacobian(x_);
+  cout << "H: " << Hj << endl;
   // VectorXd y = z - Hj * x_polar;
-  VectorXd y = z - Hj * x_;
+  VectorXd y = z - x_polar;
   cout << "y: " << y << endl;
+  cout << "R: " << R_Radar_ << endl;
   MatrixXd Ht = Hj.transpose();
-  cout << "Ht: " << Ht << endl;
-  cout << "P_: " << P_ << endl;
-  cout << "R_: " << R_Radar_ << endl;
+  
 
   MatrixXd S = Hj * P_ * Ht + R_Radar_;
-  cout << "S: " << S << endl;
-  MatrixXd Si = S.inverse();
-  cout << "Si: " << Si << endl;
-  MatrixXd K =  P_ * Ht * Si;
-  cout << "K: " << K << endl;
+  MatrixXd K =  P_ * Ht * S.inverse();
+  MatrixXd I = MatrixXd::Identity(x_.size(), x_.size());
+
 
   //new state
   x_ = x_ + (K * y);
-  cout << "x_: " << x_ << endl;
-  long x_size = x_.size();
-  MatrixXd I = MatrixXd::Identity(x_size, x_size);
   P_ = (I - K * Hj) * P_;
-  cout << "P_: " << P_ << endl;
+  cout << "UpdateEKF output: " << endl;
+  cout << "output: x_: " << x_ << endl;
+  cout << "output: P_: " << P_ << endl;
 }
